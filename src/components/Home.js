a538d12afcf2616b750c16132a7c392b1f6c46e9
minhtungo <loducfc@gmail.com>
2022-02-28 03:22:28 -0500

    added section part

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+const Home = () => {
+  return (
+    <Container>
+      <Section
+        title='Model S'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-s.jpg'
+        leftBtnText="Customer order"
+        rightBtnText='Existing inventory'
+      />
+      <Section />
+      <Section />
+      <Section />
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
